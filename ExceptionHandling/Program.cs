eef012bfecaf5f4d160d36d65ef0e1f4c0b820cc
Md. Shihab <shihabrayhan8@gmail.com>
2021-09-24 11:33:33 +0600

    Program completed....

--- ExceptionHandling/Program.cs
+++ ExceptionHandling/Program.cs
@@ -6,7 +6,25 @@ namespace ExceptionHandling
     {
         static void Main(string[] args)
         {
-            
+            //start try block
+            try
+            {
+                Console.Write("Enter a number: ");
+
+                var num = int.Parse(Console.ReadLine());
+
+                Console.WriteLine($"Squre of {num} is {num * num}");
+            }
+            //catch block
+            catch(Exception ex)
+            {
+                Console.WriteLine("Error info: " + ex.Message);
+            }
+            //finally block
+            finally
+            {
+                Console.WriteLine("Re-try with a different number.");
+            }
         }
     }
 }
